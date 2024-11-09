7f6232deba60558bea5ca6d581c38118e90873ce
Mohammed N <mnaser11218@gmail.com>
2024-11-08 21:42:10 -0500

    removed unneccessary code in service.js

--- src/main/java/com/example/demo/PersonConfig.java
+++ src/main/java/com/example/demo/PersonConfig.java
@@ -13,12 +13,12 @@ public class PersonConfig {
     @PostConstruct
     public void setup() {
         Person person1 = new Person();
-        person1.setFirstName("Leon");
-        person1.setLastName("Hunter");
+        person1.setFirstName("testing first name 1");
+        person1.setLastName("testing last name");
 
         Person person2 = new Person();
-        person2.setFirstName("John");
-        person1.setLastName("Doe");
+        person2.setFirstName("test 2");
+        person1.setLastName("test last 2");
 
         repository.saveAll(Arrays.asList(
                 person1,
